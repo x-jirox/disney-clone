016385d575bbf73534b04001b9d19958bb57e553
x-jirox <correalenin045gmail.com>
2025-03-06 20:10:55 -0500

    Disney + category and details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,97 @@
 import styled from "styled-components";
-import React from 'react'
 import ImgSlider from "./ImgSlider";
-import Viewers from "./Viewers";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
 import Recommends from "./Recommends";
+import Trending from "./Trending";
+import Viewers from "./Viewers";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { collection, onSnapshot } from "firebase/firestore"; // ✅ Importación correcta
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = (props) => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+
+  useEffect(() => {
+    console.log("Fetching movies from Firestore...");
+
+    const moviesCollection = collection(db, "movies"); // ✅ Accede a la colección correctamente
+
+    const unsubscribe = onSnapshot(moviesCollection, (snapshot) => {
+      let recommends = [];
+      let newDisneys = [];
+      let originals = [];
+      let trending = [];
+
+      snapshot.docs.forEach((doc) => {
+        switch (doc.data().type) {
+          case "recommend":
+            recommends.push({ id: doc.id, ...doc.data() });
+            break;
+
+          case "new":
+            newDisneys.push({ id: doc.id, ...doc.data() });
+            break;
+
+          case "original":
+            originals.push({ id: doc.id, ...doc.data() });
+            break;
+
+          case "trending":
+            trending.push({ id: doc.id, ...doc.data() });
+            break;
+
+          default:
+            break;
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+
+    return () => unsubscribe(); // ✅ Limpieza del listener al desmontar el componente
+  }, [userName, dispatch]);
 
   return (
     <Container>
-        <ImgSlider/>
-        <Viewers/>
-        <Recommends/>
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.main`
-position: relative;
-min-height: calc(100vh - 250px);
-overflow-x: hidden;
-display: block;
-top: 72px;
-padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 
-&:after{
-background: url("/images/home-background.png") center center / cover 
-no-repeat fixed;
-content: "";
-position: absolute;
-inset: 0px;
-opacity: 1;
-z-index: -1;
-}
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
 `;
 
-
-
-export default  Home;
+export default Home;
